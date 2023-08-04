af9388e6d43bb1759a7a0bbf7a0534af95d354fb
Hrachya Gumroyan <hgumroyan@iponweb.net>
2023-08-04 13:31:25 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -50,14 +50,21 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
   ]
    
+  SITE_ID = 1
+
   LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -116,6 +123,10 @@ class Dev(Configuration):
 
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
   ACCOUNT_ACTIVATION_DAYS = 7
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
   
   TEMPLATES = [
       {
