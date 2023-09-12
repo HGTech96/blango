fd99436fb222a66f211c75f1b9ed2ed9b01d6a11
Hrachya Gumroyan <hgumroyan@iponweb.net>
2023-09-12 12:30:00 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -64,6 +64,17 @@ class Dev(Configuration):
       'rest_framework',
   ]
    
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
+  }
+
   SITE_ID = 1
 
   LOGGING = {
