e80a1e83b360a1efa3843ec09ad484cc44f3ee30
Hrachya Gumroyan <hgumroyan@iponweb.net>
2023-09-13 10:55:52 +0000

    Final permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,12 +2,11 @@ from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 
 from blog.models import Post
-<<<<<<< HEAD
+
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-=======
+
 from blango_auth.models import User
 from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
->>>>>>> e5a7c7f64a8db5e4d9987bb8711c1cd5d757a45a
 
 
 class PostList(generics.ListCreateAPIView):
@@ -17,11 +16,7 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-<<<<<<< HEAD
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
-=======
-    # permission_classes = [AuthorModifyOrReadOnly | IsAdminUser]
->>>>>>> e5a7c7f64a8db5e4d9987bb8711c1cd5d757a45a
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
 
